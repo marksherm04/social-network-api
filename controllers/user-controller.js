b9e5bea5b281ce650d7bdd3e75fe76cc7d05a507
Mark S <marksherm04@gmail.com>
2021-08-12 15:52:49 -0400

    user-controller.js completed

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -10,7 +10,7 @@ const userController = {
 		})
 		.select('-__v')
 		.sort({ _id: -1 })
-		.then(dbUserData => res.json(dbPizzaData))
+		.then(dbUserData => res.json(dbUserData))
 		.catch(err => {
 			console.log(err);
 			res.status(500).json(err);
@@ -38,5 +38,35 @@ const userController = {
 		})
 	},
 	// create User
-	
-}
+	createUser({ body }, res) {
+		User.create(body)
+		.then(dbUserData => res.json(dbUserData))
+		.catch(err => res.status(400).json(err));
+	},
+	// update User by Id
+	updateUser({ params, body }, res) {
+		User.findOneAndUpdate({ _id: params.id }, body, { new: true })
+		.then(dbUserData => {
+			if(!dbUserData) {
+				res.status(404).json({ message: "No user found with this Id" });
+				return;
+			}
+			res.json(dbUserData);
+		})
+		.catch(err => res.status(400).json(err));
+	},
+	// delete User
+	deleteUser({ params }, res) {
+		User.findOneAndDelete({ _id: params.id })
+		.then(dbUserData => {
+			if(!dbUserData) {
+				res.status(404).json({ message: 'No user found with this Id' })
+				return;
+			}
+			res.json(dbUserData);
+		})
+		.catch(err => res.status(400).json(err));
+	}
+};
+
+module.exports = userController;
