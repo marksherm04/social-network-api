1fee80500f9efcf58b023915832a3ef4c8cd6eca
Mark S <marksherm04@gmail.com>
2021-08-22 10:39:52 -0400

    fixed issue in server.js and corrected insomnia route

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -39,6 +39,7 @@ const userController = {
 	},
 	// create User
 	createUser({ body }, res) {
+		console.log(body);
 		User.create(body)
 			.then(dbUserData => res.json(dbUserData))
 			.catch(err => res.status(400).json(err));
